894cac630f39a935e965e4a4b7643263fe3a6107
Garvit Gupta <garvit0610@outlook.com>
2017-11-20 16:12:01 +0530

    Updated ndt_india.sh and ndt_india_jitter.sh (#7)
    Updates ndt_india.sh and ndt_india_jitter.sh for new big query table names. The outdated ps2raster command replaced with psconvert command.

--- ndt_india_jitter.sh
+++ ndt_india_jitter.sh
@@ -46,7 +46,20 @@ for ((year=9; year<=input_endyear; year++)); do
  					continue
 				else
 				#BigQuery to get data from M-Lab server.
-				bq query --max_rows 1000000 "SELECT connection_spec.client_geolocation.longitude AS 				LONGITUDE,connection_spec.client_geolocation.latitude AS LATITUDE,web100_log_entry.snap.RTTVar AS JITTER, web100_log_entry.snap.RemAddress AS IP,connection_spec.client_hostname AS NAME FROM [measurement-lab:plx.google:m_lab.20${year2}_${month2}.all] WHERE IS_EXPLICITLY_DEFINED(web100_log_entry.connection_spec.remote_ip) AND IS_EXPLICITLY_DEFINED(web100_log_entry.log_time) AND project =0 AND connection_spec.client_geolocation.country_code = 'IN' AND connection_spec.client_geolocation.latitude NOT BETWEEN 19.99 AND 20.01  AND connection_spec.client_geolocation.longitude NOT BETWEEN 76.99 AND 77.01 GROUP BY IP,NAME,LONGITUDE,LATITUDE,JITTER ORDER BY JITTER DESC;" > $path/20${year2}_${month}.csv;
+				bq query --max_rows 1000000 "SELECT connection_spec.client_geolocation.longitude AS	LONGITUDE,
+												connection_spec.client_geolocation.latitude AS LATITUDE,
+												web100_log_entry.snap.RTTVar AS JITTER, 
+												web100_log_entry.snap.RemAddress AS IP,
+												connection_spec.client_hostname AS NAME 
+											FROM plx.google:m_lab.20${year2}_${month2}.all 
+											WHERE IS_EXPLICITLY_DEFINED(web100_log_entry.connection_spec.remote_ip) 
+												AND IS_EXPLICITLY_DEFINED(web100_log_entry.log_time) 
+												AND project =0 
+												AND connection_spec.client_geolocation.country_code = 'IN' 
+												AND connection_spec.client_geolocation.latitude NOT BETWEEN 19.99 AND 20.01
+												AND connection_spec.client_geolocation.longitude NOT BETWEEN 76.99 AND 77.01 
+											GROUP BY IP,NAME,LONGITUDE,LATITUDE,JITTER 
+											ORDER BY JITTER DESC;" > $path/20${year2}_${month}.csv;
 				#grep, sed, cut and cat commands are to clean up and retain only the data in the .csv file - removes 					column names, waiting statements, delimiters.
 				grep -v ^+ $path/20${year2}_${month}.csv | sponge $path/20${year2}_${month}.csv				
 				sed -i '1d' $path/20${year2}_${month}.csv
@@ -59,7 +72,7 @@ for ((year=9; year<=input_endyear; year++)); do
 				#pstext plots the month and year on top of the India map.
 				echo "4.5 8.15 33 1 5 BC $month 20$year2" | pstext -R0/11/0/8.5 -Jx1i -O >> $path/20${year2}_${month}.ps;
 								
-				ps2raster  $path/20${year2}_${month}.ps -Tj
+				psconvert $path/20${year2}_${month}.ps -Tj
 				five_digit_representation=$(printf "%05d" $month)
 				mv $path/20${year2}_${month}.jpg $path/20${year2}_${five_digit_representation}.jpg
 				echo Finished month $month of year 20$year2
@@ -80,7 +93,20 @@ for ((year=9; year<=input_endyear; year++)); do
 				then
  					continue
 				else
-				bq query --max_rows 1000000 "SELECT connection_spec.client_geolocation.longitude AS 				LONGITUDE,connection_spec.client_geolocation.latitude AS LATITUDE,web100_log_entry.snap.RTTVar AS JITTER, web100_log_entry.snap.RemAddress AS IP,connection_spec.client_hostname AS NAME FROM [measurement-lab:plx.google:m_lab.20${year2}_${month2}.all] WHERE IS_EXPLICITLY_DEFINED(web100_log_entry.connection_spec.remote_ip) AND IS_EXPLICITLY_DEFINED(web100_log_entry.log_time) AND project =0 AND connection_spec.client_geolocation.country_code = 'IN' AND connection_spec.client_geolocation.latitude NOT BETWEEN 19.99 AND 20.01  AND connection_spec.client_geolocation.longitude NOT BETWEEN 76.99 AND 77.01 GROUP BY IP,NAME,LONGITUDE,LATITUDE,JITTER ORDER BY JITTER DESC;" > $path/20${year2}_${month}.csv;
+				bq query --max_rows 1000000 "SELECT connection_spec.client_geolocation.longitude AS	LONGITUDE,
+												connection_spec.client_geolocation.latitude AS LATITUDE,
+												web100_log_entry.snap.RTTVar AS JITTER,
+												web100_log_entry.snap.RemAddress AS IP,
+												connection_spec.client_hostname AS NAME 
+											FROM plx.google:m_lab.20${year2}_${month2}.all 
+											WHERE IS_EXPLICITLY_DEFINED(web100_log_entry.connection_spec.remote_ip) 
+												AND IS_EXPLICITLY_DEFINED(web100_log_entry.log_time) 
+												AND project =0 
+												AND connection_spec.client_geolocation.country_code = 'IN' 
+												AND connection_spec.client_geolocation.latitude NOT BETWEEN 19.99 AND 20.01  
+												AND connection_spec.client_geolocation.longitude NOT BETWEEN 76.99 AND 77.01 
+											GROUP BY IP,NAME,LONGITUDE,LATITUDE,JITTER 
+											ORDER BY JITTER DESC;" > $path/20${year2}_${month}.csv;
 				grep -v ^+ $path/20${year2}_${month}.csv | sponge $path/20${year2}_${month}.csv
 				sed -i '1d' $path/20${year2}_${month}.csv
 				cut -d " " -f 2- $path/20${year2}_${month}.csv | sponge $path/20${year2}_${month}.csv
@@ -88,7 +114,7 @@ for ((year=9; year<=input_endyear; year++)); do
 				pscoast -R67/98/8/37 -JM6i -P -N1 -B5g5 -Gwhite -Sblue -Na -W -p135/40 -Cwhite -Xf -Yf -K> $path/20${year2}_${month}.ps
 				gmt psxyz $path/20${year2}_${month}.csv -R67/98/8/37/0/100 -Jx0.5c -Jz0.01 -So0.18c -Ggreen -W -p135/40/100 -L -K -O -P -Xf -Yf >> $path/20${year2}_${month}.ps;
 				echo "4.5 8.15 33 1 5 BC $month 20$year2" | pstext -R0/11/0/8.5 -Jx1i -O >> $path/20${year2}_${month}.ps;
-				ps2raster  $path/20${year2}_${month}.ps -Tj
+				psconvert $path/20${year2}_${month}.ps -Tj
 				five_digit_representation=$(printf "%05d" $month)
 				mv $path/20${year2}_${month}.jpg $path/20${year2}_${five_digit_representation}.jpg
 				echo Finished month $month of year 20$year2
